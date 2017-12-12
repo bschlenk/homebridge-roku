4c3b5cfa8dfe48e8463029dfba6011a4b389f53f
Brian Schlenker <bschlenk@umich.edu>
2017-12-11 20:28:00 -0800

    Use sample-config if global config does not exist

--- scripts/setup.js
+++ scripts/setup.js
@@ -6,15 +6,25 @@ const { generateConfig, mergeConfigs, HOMEBRIDGE_CONFIG } = require('../src/setu
 
 const rootDir = path.join(__dirname, '..');
 const configFile = path.join(rootDir, 'config.json');
+const sampleConfig = path.join(__dirname, 'sample-config.json');
 
 if (fs.existsSync(configFile)) {
   console.log('using existing config.json');
   process.exit(0);
 }
 
-generageConfig().then((config) => {
+generateConfig().then((config) => {
   console.log('generating config.json... ensure your Roku device is powered on');
-  const merged = mergeConfigs(HOMEBRIDGE_CONFIG, config);
+  let merged;
+  if (fs.existsSync(HOMEBRIDGE_CONFIG)) {
+    merged = mergeConfigs(HOMEBRIDGE_CONFIG, config);
+  } else {
+    console.log(`${HOMEBRIDGE_CONFIG} does not exist, using sample config`);
+    merged = mergeConfigs(sampleConfig, config);
+  }
   fs.writeFileSync(configFile, JSON.stringify(merged, null, 4));
+}).catch(err => {
+  console.error('failed to configure development config file', err);
+  process.exit(1);
 });
 
