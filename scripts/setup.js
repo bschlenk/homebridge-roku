f621a030e3d735317781e429d42f50be11f32bb3
Brian Schlenker <bschlenk@umich.edu>
2017-12-28 17:57:23 -0800

    Fix eslint errors

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const { generateConfig, mergeConfigs, HOMEBRIDGE_CONFIG } = require('../src/setup');
