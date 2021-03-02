aa89f62bc781901d2717e983ac23f1d765549b56
Brian Schlenker <bschlenk@umich.edu>
2021-03-01 23:01:56 -0500

    fix: typo in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   extends: [
     'eslint:recommended',
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
 
